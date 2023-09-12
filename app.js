bc3ce934db38f819f77de3a905f01ae15d0171ef
vieiraBeh <izabelcareno12345@gmail.com>
2023-09-12 14:29:23 -0400

    apenas anotações

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//Controller{
+  // A "/" é uma rota que abre a primeira página
 app.get('/', (req, res) => {
   res.render('index', { message: 'Olá, Mundo!' });
+});
+//}
+
+app.get('/', (req, res) => {
+  res.render('contato', { message: 'Meu instagram para contato @iza.careno' });
 });
 
 app.listen(port, () => {
