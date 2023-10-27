b82a8951876e6e86aa5477ae7a675b261f2244ff
carenoizabel <izabelcareno12345@gmail.com>
2023-10-27 16:24:21 -0400

    Adicionando meu projeto
    part1. não terminei ainda então terminarei na part2.  nessa primeira parte eu já estilizei as duas páginas, adicionei a imagem dos produtos (não finalizado), adicionei descrição, preço e imagem em cada, criei nova rota para produto

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ app.use(express.static('public'));
 
 //Controller{
   // A "/" é uma rota que abre a primeira página
-app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
-});
-//}
-
-app.get('/', (req, res) => {
-  res.render('contato', { message: 'Meu instagram para contato @iza.careno' });
-});
+  app.get('/', (req, res) => {
+    res.render('index');
+  });
+  
+  app.get('/produto', (req, res) => {
+    const produto = {
+      nome: 'Noite Estrelada',
+      imagem: '/noiteestrelada.jpg',
+      descricao: '"Noite Estrelada" é uma das obras de arte mais famosas do pintor holandês Vincent van Gogh. Ele pintou essa obra-prima em junho de 1889, enquanto estava internado no hospital psiquiátrico Saint-Paul-de-Mausole, em Saint-Rémy-de-Provence, na França. A pintura representa uma vista noturna da paisagem ao redor do hospital, vista da janela de seu quarto.',
+      preco: 'R$ 1.989,00',
+    };
+  
+    res.render('produto', { produto });
+  });
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
